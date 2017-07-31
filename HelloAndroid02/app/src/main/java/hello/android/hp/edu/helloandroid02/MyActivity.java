d3ad73aefbcb928839fdfb3e120683f166bf5e80
herdin <herdin86@gmail.com>
2017-07-31 15:17:46 +0900

    mod

--- HelloAndroid02/app/src/main/java/hello/android/hp/edu/helloandroid02/MyActivity.java
+++ HelloAndroid02/app/src/main/java/hello/android/hp/edu/helloandroid02/MyActivity.java
@@ -2,16 +2,12 @@ package hello.android.hp.edu.helloandroid02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        TextView tv = new TextView(this);
-//        tv.setText("Hello");
-//        setContentView(tv);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_my);
     }
 }
